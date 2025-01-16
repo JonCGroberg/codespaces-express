56d6c2bf10278a8081190ebf157dbd682776d1c1
Jonathan Groberg <jongroberg@gmail.com>
2025-01-16 21:27:07 +0000

    simple backend added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/', (request,response) => {
+  response.send("Hello World!")
+});
+
+app.listen(3000, () => {console.log(`Listening on port ${3000}`)
+} )
+
+
+
